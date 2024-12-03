10a98193da8a216926035c6448039dccd6ecac41
AFK-Shark <159244757+AFK-Shark@users.noreply.github.com>
2024-12-03 19:34:09 +0300

    statemachine

---
+++ Assets/_Source/Player/PlayerStateSwitcher.cs
@@ -0,0 +1,45 @@
+﻿using StateMachineKT.Player.PlayerStates;
+using Zenject;
+
+namespace StateMachineKT.Player
+{
+    public class PlayerStateSwitcher : IInitializable
+    {
+        private readonly PlayerInput _playerInput;
+        private readonly PlayerStateMachine _playerStateMachine;
+        
+        [Inject]
+        public PlayerStateSwitcher(PlayerInput playerInput, PlayerStateMachine playerStateMachine)
+        {
+            _playerInput = playerInput;
+            _playerStateMachine = playerStateMachine;
+        }
+
+        ~PlayerStateSwitcher() => Expose();
+        
+        public void Initialize()
+        {
+            Bind();
+        }
+
+        private void SwitchStates()
+        {
+            switch (_playerStateMachine.CurrentState)
+            {
+                case InvisibleState:
+                    _playerStateMachine.SwitchState<ShootState>();
+                    break;
+                case ShootState:
+                    _playerStateMachine.SwitchState<RedCircleState>();
+                    break;
+                case RedCircleState:
+                    _playerStateMachine.SwitchState<InvisibleState>();
+                    break;
+            }
+        }
+
+        private void Bind() => _playerInput.OnStateSwitch += SwitchStates;
+
+        private void Expose() => _playerInput.OnStateSwitch -= SwitchStates;
+    }
+}
